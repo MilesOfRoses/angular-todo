7f4305194170cc57cc085c8967ad62ce21fba1ff
Miles Rose <Milesroseproductions@gmail.com>
2019-10-21 21:28:33 -0400

    the second part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// make it so that the filter checks eac time a character is entered (think google autofill )
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -22,6 +24,20 @@ export class AppComponent {
 
   newTodo = ''; // for the storage of data on the "new interface"
 
+  filterText: string;
+
+  filteredTodos = [...this.todos]; // spread operator. *TAKEN FROM FAMILY MEMBER PROJECT*
+
+  filterTodos() {
+    console.log('the filter funcion has run');
+    this.filteredTodos = this.todos.filter(
+      item => item.name.toLowerCase() === this.filterText.toLowerCase() // toLowerCase() makes the filter not be case sensitive
+    );
+    if (!this.filterText) {
+      this.filteredTodos = [...this.todos];
+    }
+  }
+
   remain = function() {
     let c = 0; // initializing the new counter incrementor variable 'c'
     this.todos.forEach(t => { // loop through this array using letter 't' as the iterator, since the parrent object is "todos"
@@ -33,23 +49,21 @@ export class AppComponent {
   };
 
   del = function(i) {
-    this.todos.splice(i, 1); // privide it the index (i), and splice out 1 single item
-  };
-  filterCompleted = function() { // this function removes everything that was flagged as completed.
+    this.todos.splice(i, 1); // privide it the index (i), and splice out 1 single item *update* changed "filteredTodos" to "todos"
+    this.filteredTodos.splice(i, 1); // privide it the index (i), and splice out 1 single item *update* changed "filteredTodos" to "todos"
+    console.log('the delete function has run');
     const oldTodos = this.todos; // holding an old todos variable for the current dataset
-    this.todos = []; // empty out the array
+    // this.todos = []; // empty out the array
     oldTodos.forEach(t => { // loop through this array
-      if (!t.done) { // if the todo is not done..
-        this.todos.push(t); // then push into the array each item that has been completed.
-        // console.log(" this is where the strikethrough code should go ");
-      }
     });
   };
 
   add = function() {
     const t = {name: this.newTodo, done: false}; // false is the default value, this is how new items will initially be created
-    this.todos.push(t);
+    this.filteredTodos.push(t); // for the add button
+    this.todos.push(t); // for the filter feature... IDK why this works but I'll take it
     this.newTodo = '';
+    console.log('something should have been added');
   };
 }
 
